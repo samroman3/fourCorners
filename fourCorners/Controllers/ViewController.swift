72da7eb4b183d5a3f27ef4db3fb8469d9d54847a
Sam <samroman3@gmail.com>
2019-09-16 10:55:22 -0400

    Adds outlets and initial functions to VC

--- fourCorners/Controllers/ViewController.swift
+++ fourCorners/Controllers/ViewController.swift
@@ -9,8 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    //MARK: Outlet Collection
+    
+    @IBOutlet var collection: [UIButton]!
+    
+
+    @IBAction func buttonPress(_ sender: UIButton) {
+        //Switch to Increase score based on sender.tag of player button
+        
+//        switch sender.tag {
+//        case 1:
+//            Players.playerOne.increaseScore(player: 1)
+//        case 2:
+//            Players.playerTwo.increaseScore(player: 2)
+//        case 3:
+//            Players.playerThree.increaseScore(player: 3)
+//        case 4:
+//            Players.playerFour.increaseScore(player: 4)
+//        default:
+//           return
+//        }
+    
+    }
+    
+    
+    
+    @IBOutlet weak var promptLabel: UILabel!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    
+    func gameStart(){
+        self.startButton.isEnabled = false
+        promptLabel.isHidden = false
+        promptLabel.text? = Prompt.mvpPromptArray.randomElement()!.prompt
+        
+    }
+    
+    func gameOver(){
+        self.startButton.isHidden = false
+        self.startButton.isEnabled = true
+        
+    }
+    
+//    func checkWin(){
+//        Players.winningCondition(<#T##Players#>)
+//    }
+//
+    
     override func viewDidLoad() {
+        promptLabel.text = ""
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
