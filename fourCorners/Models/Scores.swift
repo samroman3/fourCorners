910d035d715bb418798ec4118ddd23a93b4a0693
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-16 10:36:30 -0400

    refactors Player class to remove round property

--- fourCorners/Models/Scores.swift
+++ fourCorners/Models/Scores.swift
@@ -9,50 +9,42 @@
 import Foundation
 import UIKit
 
-class  Players{
+class Players {
     
-    var name:String
-    var score:Int
-    var round:Int
+    var name: String
+    var score: Int
 
-    init (name:String, score:Int, round:Int) {
+    init (name: String, score: Int) {
         self.name = name
         self.score = score
-        self.round = round
     }
-    let playerOne = Players(name: "Player One", score: 0, round: 0)
-    let playerTwo = Players(name: "Player Two", score: 0, round: 0)
-    let playerThree = Players(name: "Player Three", score: 0, round: 0)
-    let playerFour = Players(name: "Player Four", score: 0, round:0)
     
-    let players:[Players] = []
+    let playerOne = Players(name: "Player One", score: 0)
+    let playerTwo = Players(name: "Player Two", score: 0)
+    let playerThree = Players(name: "Player Three", score: 0)
+    let playerFour = Players(name: "Player Four", score: 0)
     
-    func increaseScore(player:Int) {
+    let players: [Players] = []
+    
+    func increaseScore (player: Int) {
         switch player {
-        case 1:
-            playerOne.score += 1
-            playerOne.round += 1
-        case 2:
-            playerTwo.score += 1
-            playerTwo.round += 1
-            
-        case 3:
-            playerThree.score += 1
-            playerThree.round += 1
-        case 4:
-            playerFour.score += 1
-            playerFour.round += 1
-            
-        default:
-            print("not a player")
+            case 1:
+                playerOne.score += 1
+            case 2:
+                playerTwo.score += 1
+            case 3:
+                playerThree.score += 1
+            case 4:
+                playerFour.score += 1
+            default:
+                print("not a player")
         }
     }
+    
     func winningCondition() -> Players {
-        
-        
-            
-           let playersRound = [playerOne.round,playerTwo.round,playerThree.round,playerFour.round].reduce(0,+)
-        if playersRound >= 5 {
+    
+        let roundsPassed = [playerOne.score,playerTwo.score,playerThree.score,playerFour.score].reduce(0,+)
+        if roundsPassed >= 5 {
             let players = [playerOne,playerTwo,playerThree,playerFour].sorted(by: {$0.score > $1.score} )
         }
          return players[0]
