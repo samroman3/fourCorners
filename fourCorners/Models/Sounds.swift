f5eb20e1709cefbc6f641b1cbbbd9e24dddbd164
phoenixmcknight <phoenixmcknight@gmail.com>
2019-09-17 17:49:00 -0400

    adds sounds to Sound file

--- fourCorners/Models/Sounds.swift
+++ fourCorners/Models/Sounds.swift
@@ -65,6 +65,44 @@ struct Sounds {
                     }
                     return sound
 }
-   
+    static func setTimerSound() -> AVAudioPlayer {
+        var sound = AVAudioPlayer()
+        let path = Bundle.main.path(forResource: "timeAudioSave.wav", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            //create your audioPlayer in your parent class as a property
+            sound = try AVAudioPlayer(contentsOf: url)
+        } catch {
+            print("couldn't load the file")
+        }
+        return sound
+    }
+    static func setMenuSound() -> AVAudioPlayer {
+        var sound = AVAudioPlayer()
+        let path = Bundle.main.path(forResource: "menuSave.wav", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            //create your audioPlayer in your parent class as a property
+            sound = try AVAudioPlayer(contentsOf: url)
+        } catch {
+            print("couldn't load the file")
+        }
+        return sound
+    }
+//    static func setPressSound() -> AVAudioPlayer {
+//        var sound = AVAudioPlayer()
+//        let path = Bundle.main.path(forResource: "synthThree.wav", ofType: nil)!
+//        let url = URL(fileURLWithPath: path)
+//        
+//        do {
+//            //create your audioPlayer in your parent class as a property
+//            sound = try AVAudioPlayer(contentsOf: url)
+//        } catch {
+//            print("couldn't load the file")
+//        }
+//        return sound
+//    }
 
 }
