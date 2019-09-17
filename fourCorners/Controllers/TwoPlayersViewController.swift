b9b2ce8776bb29248062f30b00672664fb234dc3
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-17 12:04:08 -0400

    adds remaining objects to storyboard VCs and constaints

--- fourCorners/Controllers/TwoPlayersViewController.swift
+++ fourCorners/Controllers/TwoPlayersViewController.swift
@@ -10,22 +10,16 @@ import UIKit
 
 class TwoPlayersViewController: UIViewController {
 
+    
+    @IBOutlet weak var topButtonImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
-
-        // Do any additional setup after loading the view.
+        configureImages()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureImages() {
+        topButtonImage.image = UIImage(named: "playerOneButton" )
     }
-    */
-
 }
